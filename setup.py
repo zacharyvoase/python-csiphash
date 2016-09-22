4e1da279e6d88db1cee536fcf012861d254ce74e
Zachary Voase <zack@meat.io>
2016-09-22 16:50:32 -0700

    Get the long description from README.rst.

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
+from codecs import open
+from os import path
+
 from setuptools import setup, find_packages
+
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    readme = f.read()
 
 
 setup(
@@ -6,6 +16,7 @@ setup(
     version='0.0.1',
 
     description='A CFFI-based implementation of SipHash24',
+    long_description=readme,
     url='https://github.com/zacharyvoase/python-csiphash',
 
     author='Zachary Voase',
